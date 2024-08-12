557bec63c121089e1b39df30d5e3f5a169a4307e
Petermwansa <mwansapeter800@gmail.com>
2024-08-12 17:27:48 +0300

    change to serverSide revalidation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5dc463443ceeae39"; }
+/******/ 		__webpack_require__.h = function() { return "ac29e0337efa8972"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -30,32 +30,12 @@ function HomePage(props) {
     )
 }
 
-// // this can be used to revalidate the data on the server side everytime there is a change 
-// // use this method if data changes multiple times every second 
-// export async function getServerSideProps(context) {
-//     const req = context.req;
-//     const res = context.res;
+// this can be used to revalidate the data on the server side everytime there is a change 
+// use this method if data changes multiple times every second 
+export async function getServerSideProps(context) {
+    const req = context.req;
+    const res = context.res;
 
-
-//     //fetch data from the API
-
-//     return {
-//         props: {
-//             meetups: DUMMY_MEETUPS
-//         }
-//     }
-//     // you cannot set revalidate because there's no need 
-// }
-
-
-
-
-// by default next js generates the pages statically but if you need
-// to wait for data you can do so by exporting a special function 
-export async function getStaticProps() {
-
-    // here we fetch the data from the Api
-    // Uo90nWzMpoicRTeG 
     const client = await MongoClient.connect(
         'mongodb+srv://user_1:Uo90nWzMpoicRTeG@meetupnext0.xpkke.mongodb.net/meetups?retryWrites=true&w=majority&appName=MeetupNext0'
     );
@@ -68,9 +48,9 @@ export async function getStaticProps() {
     client.close();
 
 
-    // the code here never ends up on the client side because it is executed in the build 
-    //you can fetch and read data here 
-    // you always return an obj here 
+
+    //fetch data from the API
+
     return {
         props: {
             meetups: meetups.map(meetup => ({
@@ -80,10 +60,47 @@ export async function getStaticProps() {
                 id: meetup._id.toString(),
             }))
         },
-        // if the data changes frequently we add the following to revalidate the data after every specified timeframe
-        revalidate: 10
     }
+    // you cannot set revalidate because there's no need 
 }
+
+
+
+
+// // by default next js generates the pages statically but if you need
+// // to wait for data you can do so by exporting a special function 
+// export async function getStaticProps() {
+
+//     // here we fetch the data from the Api
+//     // Uo90nWzMpoicRTeG 
+//     const client = await MongoClient.connect(
+//         'mongodb+srv://user_1:Uo90nWzMpoicRTeG@meetupnext0.xpkke.mongodb.net/meetups?retryWrites=true&w=majority&appName=MeetupNext0'
+//     );
+//     const db = client.db();
+
+//     const meetupsCollection = db.collection('meetups');
+
+//     const meetups = await meetupsCollection.find().toArray();
+
+//     client.close();
+
+
+//     // the code here never ends up on the client side because it is executed in the build 
+//     //you can fetch and read data here 
+//     // you always return an obj here 
+//     return {
+//         props: {
+//             meetups: meetups.map(meetup => ({
+//                 title: meetup.title,
+//                 address: meetup.address,
+//                 image: meetup.image,
+//                 id: meetup._id.toString(),
+//             }))
+//         },
+//         // if the data changes frequently we add the following to revalidate the data after every specified timeframe
+//         revalidate: 10
+//     }
+// }
 
 
 export default HomePage;
